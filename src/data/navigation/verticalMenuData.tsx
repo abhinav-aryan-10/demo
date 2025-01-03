0d220a019e490f244baf763469cb60cbbbf0f375
abhinav-aryan-10 <abhinav.aryan@bizacuity.com>
2025-01-03 16:10:56 +0530

    feat: initial commit

---
+++ src/data/navigation/verticalMenuData.tsx
@@ -0,0 +1,22 @@
+// Type Imports
+import type { VerticalMenuDataType } from '@/types/menuTypes'
+
+const verticalMenuData = (): VerticalMenuDataType[] => [
+  {
+    label: 'Home',
+    href: '/home',
+    icon: 'ri-home-smile-line'
+  },
+  {
+    label: 'About',
+    href: '/about',
+    icon: 'ri-information-line'
+  },
+   {
+    label: 'Contact Us',
+    href: '/contactUs',
+    icon: 'ri-phone-line'
+  }
+]
+
+export default verticalMenuData
